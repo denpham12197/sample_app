9464809b030eeeeea68ec788fecf72acda9743fd
denpham12197 <denpham12197@gmail.com>
2019-05-21 13:29:35 +0700

    chapter 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  before_save{self.email = email.downcase}
+  validates :name,  presence: true, length: {maximum: Setting.User.name_lenght}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: Setting.User.email_lenght},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: {minimum: Setting.User.password_lenght}
 end
