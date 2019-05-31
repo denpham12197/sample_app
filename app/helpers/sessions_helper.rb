e47933925b4890b2a849fc224f8d3a4d0e667a64
denpham12197 <denpham12197@gmail.com>
2019-05-31 16:27:51 +0700

    Chapter 11: Account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,16 +3,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user? user
-    user == current_user
-  end
-
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user&.authenticated?(cookies[:remember_token])
+      if user&.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -29,8 +25,13 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  def forget _user
-    cookies.delete [:user_id, :remember_token]
+  def current_user? user
+    user == current_user
+  end
+
+  def forget user
+    user.forget
+    cookies.delete(:user_id, :remember_token)
   end
 
   def log_out
